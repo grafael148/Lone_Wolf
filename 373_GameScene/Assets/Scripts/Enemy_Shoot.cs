daa6886d92ce12970b383127b1d76ed4eb9b0776
grafael148 <81830869+grafael148@users.noreply.github.com>
2024-04-28 16:03:50 -0700

    Added Vertical shooting to the enemy.

--- 373_GameScene/Assets/Scripts/Enemy_Shoot.cs
+++ 373_GameScene/Assets/Scripts/Enemy_Shoot.cs
@@ -11,6 +11,10 @@ public class Enemy_Shoot : MonoBehaviour
     public Transform spawnPoint;
     public float EnemySpeed;
 
+    /// this code is so the enemy isn't constantly shooting when the player is behind walls.
+    public Transform player;
+    private bool isPlayerHit;
+    
     private void Update()
     {
         ShootAtPlayer();
@@ -22,15 +26,34 @@ public class Enemy_Shoot : MonoBehaviour
 
         if (bulletTime <= 0)
         {
+            //this code is so the enemy isn't constantly shooting when the player is behind walls.
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, player.position - transform.position, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    isPlayerHit = true;
+                }
+            }
+
+            if (isPlayerHit)
+            {
+               
+                Debug.Log("Enemy Is Shooting");
+                GameObject bulletObj = Instantiate(EnemyBullet, spawnPoint.position, spawnPoint.rotation) as GameObject;
+                Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+
+                // shoots at the player if on a ledge
+                Vector3 direction = player.position - transform.position;
+                bulletRig.AddForce(direction * EnemySpeed);
+                bulletTime = timer; // resets timer
+              //Destroy(bulletObj, 5f); // destroy after 5 seconds
+
+                isPlayerHit = false;
+
+            }
+
             
-
-            Debug.Log("shooting enemy");
-            GameObject bulletObj = Instantiate(EnemyBullet, spawnPoint.position, spawnPoint.rotation) as GameObject;
-            Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
-
-            bulletRig.AddForce(bulletRig.transform.forward * EnemySpeed);
-            bulletTime = timer; // resets timer
-            //Destroy(bulletObj, 5f); // destroy after 5 seconds
         }
 
         
